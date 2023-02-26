d2a06d6ece99177af77e23ab05838c9e47da2537
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-26 10:59:08 +0100

    Made some changes

--- user_list-workshop/client/src/App.js
+++ user_list-workshop/client/src/App.js
@@ -1,5 +1,6 @@
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
+import { Footer } from "./components/common/Footer";
+import { Header } from "./components/common/Header";
+import "./App.css";
 
 function App() {
   return (
