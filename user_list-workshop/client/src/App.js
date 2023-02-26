770daf6869d74a44c6f93f358d4f5f973faa753b
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-26 12:48:18 +0100

    Some changes

--- user_list-workshop/client/src/App.js
+++ user_list-workshop/client/src/App.js
@@ -2,14 +2,31 @@ import { Footer } from "./components/common/Footer";
 import { Header } from "./components/common/Header";
 import "./App.css";
 import { Search } from "./components/search/Search";
+import { UserSection } from "./components/user-section/UserSection";
+import { useEffect, useState } from "react";
+
+const baseUrl = "http://localhost:3005/api";
 
 function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch(`${baseUrl}/users`)
+            .then((response) => response.json())
+            .then((result) => {
+                setUsers(result.users);
+            });
+    }, []);
+
+    console.log(users);
+
     return (
         <div className="App">
             <Header />
             <main className="main">
                 <section className="card users-container">
                     <Search />
+                    <UserSection />
                 </section>
             </main>
             <Footer />
