2d4aa019f0ff895171b6af515995c6037961117a
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-26 11:30:19 +0100

    Created Search component

--- user_list-workshop/client/src/App.js
+++ user_list-workshop/client/src/App.js
@@ -1,15 +1,20 @@
 import { Footer } from "./components/common/Footer";
 import { Header } from "./components/common/Header";
 import "./App.css";
+import { Search } from "./components/search/Search";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main className="main"></main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+                </section>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
