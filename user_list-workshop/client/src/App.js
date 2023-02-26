e3ec91d104cfe85e7120830d65c6de96da94f4a9
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-02-26 15:40:07 +0100

    works

--- user_list-workshop/client/src/App.js
+++ user_list-workshop/client/src/App.js
@@ -1,30 +1,21 @@
 import { useEffect, useState } from "react";
 
-<<<<<<< HEAD
-=======
-import * as userServive from "./services/UserService";
-
 import { Footer } from "./components/common/Footer";
 import { Header } from "./components/common/Header";
 import { Search } from "./components/search/Search";
 import { UserList } from "./components/user-list/UserList";
 import "./App.css";
 
->>>>>>> bf229eb96f652583bd4d0d37693c1004ca3d9072
 function App() {
     const [users, setUsers] = useState([]);
     const baseUrl = "http://localhost:3005/api/users";
 
     useEffect(() => {
-<<<<<<< HEAD
         fetch(baseUrl)
             .then((response) => response.json())
             .then((result) => {
                 setUsers(result.users);
             });
-=======
-        userServive.getAll().then((users) => setUsers(users));
->>>>>>> bf229eb96f652583bd4d0d37693c1004ca3d9072
     }, []);
 
     console.log(users);
